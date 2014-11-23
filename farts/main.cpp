40d55c1a974ff76a91125c1cfa97480f020d901b
Jake Cataford <jakecataford@gmail.com>
2014-11-22 20:29:17 -0500

    Initial Commit

---
+++ farts/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  farts
+//
+//  Created by Jake Cataford on 11/22/14.
+//  Copyright (c) 2014 svblm. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
